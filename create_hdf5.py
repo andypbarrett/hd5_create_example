a1a1eac8bea611a6bd160e3aabd6dffb2c34c7b6
Andy Barrett <apbarret@nsidc.org>
2022-08-19 13:04:27 -0600

    add group

--- create_hdf5.py
+++ create_hdf5.py
@@ -6,7 +6,8 @@ shape = (5, 10)
 dims = ["level", "point"]
 
 f = h5py.File("test.h5", "w")
-dset = f.create_dataset("temperature", shape=shape, dtype="f")
+grp = f.create_group("Data")
+dset = grp.create_dataset("temperature", shape=shape, dtype="f")
 
 print(dset[:,:])
 
