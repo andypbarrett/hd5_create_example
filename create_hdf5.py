173eb87cc3f205e363c59fbbe36cf7a0410d7756
Andy Barrett <apbarret@nsidc.org>
2022-08-19 12:42:06 -0600

    add open file and create dataset

--- create_hdf5.py
+++ create_hdf5.py
@@ -1,1 +1,12 @@
 '''An example script to create a multi-dimensional HDF5 file'''
+
+import h5py
+
+shape = (5, 10)
+dims = ["level", "point"]
+
+f = h5py.File("test.h5", "w")
+dset = f.create_dataset("temperature", shape=shape, dtype="f")
+
+print(dset[:,:])
+
