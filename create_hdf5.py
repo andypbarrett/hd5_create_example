3e104878a3423439e7aed96dccc5d9d271d54d2f
Andy Barrett <apbarret@nsidc.org>
2022-08-19 13:12:37 -0600

    add comment

--- create_hdf5.py
+++ create_hdf5.py
@@ -7,7 +7,11 @@ dims = ["level", "point"]
 
 f = h5py.File("test.h5", "w")
 grp = f.create_group("Data")
+
+# Create a temperature dataset with two dimensions
 dset = grp.create_dataset("temperature", shape=shape, dtype="f")
 
+# Testing
 print(dset[:,:])
-
+for attr_name, attr_value in dset.attrs.items():
+    print(f"{attr_name}: {attr_value}")
