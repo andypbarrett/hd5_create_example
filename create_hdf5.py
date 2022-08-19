4ab0479fc686245591d3e5d5c8c513bcdefe8e9a
Andy Barrett <apbarret@nsidc.org>
2022-08-19 13:22:32 -0600

    refactor using context

--- create_hdf5.py
+++ create_hdf5.py
@@ -2,18 +2,24 @@
 
 import h5py
 
+filename = "test.h5"
 shape = (5, 10)
 dims = ["level", "point"]
 
-f = h5py.File("test.h5", "w")
-grp = f.create_group("Data")
+with h5py.File(filename, "w") as f:
+    grp = f.create_group("Data")
 
-# Create a temperature dataset with two dimensions
-dset = grp.create_dataset("temperature", shape=shape, dtype="f")
-dset.attrs["long_name"] = "air_temperature"
-dset.attrs["units"] = "K"
+    # Create a temperature dataset with two dimensions in the Data group
+    dset = grp.create_dataset("temperature", shape=shape, dtype="f")
+    dset.attrs["long_name"] = "air_temperature"
+    dset.attrs["units"] = "K"
 
 # Testing
-print(dset[:,:])
-for attr_name, attr_value in dset.attrs.items():
-    print(f"{attr_name}: {attr_value}")
+with h5py.File(filename, "r") as f:
+    for grp in f:
+        print(f[grp].name)
+        for dset in f[grp]:
+            print(f[f"{grp}/{dset}"])
+            print(f[f"{grp}/{dset}"][:,:])
+            for attr_name, attr_value in f[f"{grp}/{dset}"].attrs.items():
+                print(f"{attr_name}: {attr_value}")
